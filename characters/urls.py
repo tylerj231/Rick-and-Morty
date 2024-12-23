6d9c8cc10d359480f0f27f1645af953d30f4e40a
Mykola <jacktyler2391@gmail.com>
2024-12-23 13:36:49 +0200

    Add DRF & implement 1st view

--- characters/urls.py
+++ characters/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+
+from characters.views import get_random_character_view
 
 app_name = "app"
 
-urlpatterns = []
+urlpatterns = [
+    path("characters/random/", get_random_character_view, name="character_random"),
+]
